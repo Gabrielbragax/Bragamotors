3c304a420ff691dded1e287cce0e7908b26050e6
gabrielbragax <g.bragax@gmail.com>
2026-06-12 10:19:13 -0300

    fix permissao supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://mdxpwztivordpuxxwtse.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1keHB3enRpdm9yZHB1eHh3dHNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyMzU4ODEsImV4cCI6MjA5NjgxMTg4MX0.5GPQi9NKwOIazZEVgEBMQM0wRz6PkJuctOK0nXbc8ZU'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1keHB3enRpdm9yZHB1eHh3dHNlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTIzNTg4MSwiZXhwIjoyMDk2ODExODgxfQ.oI2yrAP0XhMlrT5Kx4lWXgmsALHAtU3wOwa2pcCKQoo'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
